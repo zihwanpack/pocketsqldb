98b220d38a7ac5405fc9f36028609c3612af945f
Layton <jihwan8807@naver.com>
2024-09-12 12:04:41 +0900

    feat : useDelete 훅 구현

--- src/hooks/useDelete.ts
+++ src/hooks/useDelete.ts
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import axios, { AxiosError } from 'axios';
 
-function useDelete() {
-  return (
+function useDelete<T>(url: string, id: number) {
+  const [data, setData] = useState<T | null>(null);
+  const [isPending, setPending] = useState<boolean>(true);
+  const [error, setError] = useState<AxiosError | null>(null);
 
-  )
+  useEffect(() => {
+    const deleteData = async () => {
+      try {
+        setPending(true);
+        const res = await axios.delete(`${url}/${id}`);
+        setData(res.data);
+      } catch (err) {
+        setError(err as AxiosError);
+      } finally {
+        setPending(false);
+      }
+    };
+
+    deleteData();
+    return () => {
+      setData(null);
+      setError(null);
+    };
+  }, [url, id]);
+
+  return { data, isPending, error };
 }
 
-export default useDelete
+export default useDelete;
